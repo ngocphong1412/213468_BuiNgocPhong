0514624ae17b8346c9edf3804d9f52336a831386
NgocPhong <buingocphong@270gmail.com>
2024-10-24 10:13:04 +0700

    Them tat ca len sever

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// Khai báon đúng hàm từ các file .c khác 
+void print_shellc1();
+void print_shellc2();
+
+int main() {
+  print_shellc1(); // Gọi hàm từ shellc1.c
+  print_shellc2(); // gọi hàm từ shellc1.c
+return 0;
+}
